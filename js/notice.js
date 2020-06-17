1d4d9a3738644d3e3ddcebb96689d71f7ac0024e
Hojoon Lee <leehj.david@gmail.com>
2020-06-17 18:21:51 +0900

    notice.js add / header test add

--- js/notice.js
+++ js/notice.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-
+    $('li').hover(function(){
+        $(this).css("background-color", "#f1f1f1");
+        $(this).css("transition", "0.7s")
+    }, function(){
+        $(this).css("background-color", "white");
+        $(this).css("transition", "0.7s")
+    })
 });
