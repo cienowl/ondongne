d575aee95bbf93d3ea248ebff263851423995661
Hojoon Lee <leehj.david@gmail.com>
2020-07-22 14:59:13 +0900

    modification

--- src/com/ondongne/teampjt/circle/dao/DataAccessCircle.java
+++ src/com/ondongne/teampjt/circle/dao/DataAccessCircle.java
@@ -27,31 +27,22 @@ public class DataAccessCircle {
 		return instance;
 	}
 
-	Reader reader;
-
 	public static SqlSessionFactory getConnection() {
+		Reader reader;
+
 		try {
 			reader = Resources.getResourceAsReader("com/ondongne/teampjt/circle/mybatis_config.xml");
 			sqlFactory = new SqlSessionFactoryBuilder().build(reader);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return sqlFactory;
 
+		return sqlFactory;
 	}
 
 	public void circleInsert(DataTransferCircle dto) {
 
 		SqlSession sqlsession = sqlFactory.openSession();
-		String title =  dto.getTitle();
-		String region = dto.getRegion();
-		String zipcode = dto.getZipcode();
-		String address = dto.getAddress();
-		String address_detail = dto.getAddress_detail();
-		String event_date = dto.getEvent_date();
-		String end_date = dto.getEnd_date();
-		String gender = dto.getGender();
-		String description = dto.getDescription();
 
 		sqlsession.insert("circleInsert", dto);
 		sqlsession.commit();
--- src/com/ondongne/teampjt/sell/dao/DataAccessSell.java
+++ src/com/ondongne/teampjt/sell/dao/DataAccessSell.java
@@ -10,30 +10,30 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 public class DataAccessSell {
 
 	private static DataAccessSell instance;
-	
+
 	public static DataAccessSell getInstance() {
 		if(instance == null) {
 			synchronized (DataAccessSell.class) {
-				instance = new DataAccessSell();				
+				instance = new DataAccessSell();
 			}
-		}				
+		}
 		return instance;
 	}
-	
+
 	static SqlSessionFactory sqlFactory;
-	
+
 	public static SqlSessionFactory getConnection() {
 		Reader reader;
-		
+
 		try {
 			reader = Resources.getResourceAsReader("com/ondongne/teampjt/sell/mybatis-config.xml");
 			sqlFactory = new SqlSessionFactoryBuilder().build(reader);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return sqlFactory;
 	}
-	
+
 
 }
